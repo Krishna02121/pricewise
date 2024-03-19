bdebe536d1654bd07e920448729e39886f3dcb7b
Krishna02121 <krishnaagrawal15881@gmail.com>
2024-03-19 12:45:33 +0530

    fixing error

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,6 +10,10 @@ import {
 } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300;
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function GET() {
   try {
     connectToDB();
@@ -74,8 +78,9 @@ export async function GET() {
     );
 
     return NextResponse.json({
-      message: 'Ok', data: updatedProducts
-    })
+      message: "Ok",
+      data: updatedProducts,
+    });
   } catch (error) {
     throw new Error(`Error in GET: ${error}`);
   }
